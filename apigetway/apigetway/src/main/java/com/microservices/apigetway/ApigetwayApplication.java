459a7a395137b05d72a68e1c4ccbf65973351f12
hoan <nguyenthanhhoan300904@gmail.com>
2025-10-03 15:59:04 +0700

    fix api gatewap

--- apigetway/apigetway/src/main/java/com/microservices/apigetway/ApigetwayApplication.java
+++ apigetway/apigetway/src/main/java/com/microservices/apigetway/ApigetwayApplication.java
@@ -2,6 +2,9 @@ package com.microservices.apigetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApigetwayApplication {
@@ -10,4 +13,9 @@ public class ApigetwayApplication {
 		SpringApplication.run(ApigetwayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver keyResolver() {
+		// Lấy IP client để làm key limit
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
 }
